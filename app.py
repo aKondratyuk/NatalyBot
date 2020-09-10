2477ad9705017faeab2c6676950e1871ae4770d9
Andrii <plug2up@gmail.com>
2020-09-10 17:43:25 +0300

    Sign up/in form v1.0

--- app.py
+++ app.py
@@ -1,16 +1,23 @@
 # Imports the Flask class
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 # Creates an app and checks if its the main or imported
 app = Flask(__name__)
+Bootstrap(app)
 
 # Specifies what URL triggers hello_world()
 @app.route('/')
 # The function run on the index route
-def hello_world():
+def login():
     # Returns the text to be displayed
-    return "Hello World!"
-
+    return render_template('login.html')
 # If this script isn't an import
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+
 if __name__ == "__main__":
     # Run the app until stopped
     app.run()
