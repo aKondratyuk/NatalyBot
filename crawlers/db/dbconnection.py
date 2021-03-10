eed0647ae6320f382120e62f6409267c531b7451
Anton <antonace@users.noreply.github.com>
2021-03-10 17:39:11 +0200

    sqlalchemy relational models

---
+++ crawlers/db/dbconnection.py
@@ -0,0 +1,13 @@
+import os
+
+BASE = 'postgresql+psycopg2'
+USERNAME = os.getenv('VAR_USERNAME')
+PASSWORD = os.getenv('VAR_PASSWORD')
+HOST = os.getenv('VAR_HOST')
+PORT = '5432'
+DATABASE = os.getenv('VAR_DATABASE')
+
+connection_string = '{base}://{user}:{pw}@{host}:{port}/{db}'.format(
+    base=BASE, user=USERNAME, pw=PASSWORD,
+    host=HOST, port=PORT, db=DATABASE
+)
---
+++ crawlers/db/dbcontext.py
@@ -0,0 +1,17 @@
+from sqlalchemy import MetaData, Table, Column, String,\
+    DateTime, Boolean, Text, ForeignKey
+
+metadata = MetaData()
+account_table = Table('Account',
+                      metadata,
+                      Column('account_id', String(10), primary_key=True),
+                      Column('account_password', String(50), nullable=False))
+
+dialogue_table = Table('Dialogue',
+                       metadata,
+                       Column('dialogue_id', String(10), primary_key=True),
+                       Column('sender_id', String(10), nullable=False),
+                       Column('send_time', DateTime, nullable=False),
+                       Column('viewed', Boolean, nullable=False),
+                       Column('sender_message', Text, nullable=False),
+                       Column('receiver_id', String(10), ForeignKey('Account.account_id')))
