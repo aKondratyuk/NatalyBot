7c291472b3a3c5da4461eb8ce11f3835dbd563f0
Anton <antonace@users.noreply.github.com>
2021-03-11 19:01:03 +0200

    reaarange imports

---
+++ crawlers/main.py
@@ -0,0 +1,48 @@
+import asyncio
+import threading
+
+from crawler import Crawler
+from db import AccountService, session, create_tables
+
+
+async def crawl_single(**kwargs):
+    worker = Crawler(**kwargs)
+    await worker.index()
+
+
+async def main_event_loop(delay: float):
+    service = AccountService(session)
+    accounts = service.fetch_accounts()
+
+    await asyncio.sleep(delay)
+    await asyncio.gather(*[
+        crawl_single(auth_id=account.account_id,
+                     auth_password=account.account_password,
+                     show_new_messages=False,
+                     save_db=True) for account in accounts
+    ])
+
+
+def crawler_fire_and_forget_in_loop(master_loop, delay: float):
+    asyncio.set_event_loop(master_loop)
+
+    try:
+        master_loop.run_until_complete(main_event_loop(delay))
+    finally:
+        master_loop.run_until_complete(master_loop.shutdown_asyncgens())
+        master_loop.close()
+
+
+def crawler_fire_and_forget(delay: float, join_after_complete=False):
+    loop = asyncio.get_event_loop()
+    crawler_thread = threading.Thread(target=crawler_fire_and_forget_in_loop,
+                                      args=(loop, delay,))
+    crawler_thread.start()
+
+    if join_after_complete:
+        crawler_thread.join()
+
+
+if __name__ == '__main__':
+    create_tables()
+    crawler_fire_and_forget(delay=0.0)
