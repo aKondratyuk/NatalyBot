431ff1b0e9d3796c579de17c24b2baf1ca34904b
Anton <antonace@users.noreply.github.com>
2021-03-11 21:17:50 +0200

    show new messages flag

--- crawlers/main.py
+++ crawlers/main.py
@@ -10,7 +10,7 @@ async def crawl_single(**kwargs):
     await worker.index()
 
 
-async def main_event_loop(delay: float):
+async def main_event_loop(delay: float, show_new_messages: bool):
     service = AccountService(session)
     accounts = service.fetch_accounts()
 
@@ -18,25 +18,25 @@ async def main_event_loop(delay: float):
     await asyncio.gather(*[
         crawl_single(auth_id=account.account_id,
                      auth_password=account.account_password,
-                     show_new_messages=False,
+                     show_new_messages=show_new_messages,
                      save_db=True) for account in accounts
     ])
 
 
-def crawler_fire_and_forget_in_loop(master_loop, delay: float):
+def crawler_fire_and_forget_in_loop(master_loop, delay: float, show_new_messages: bool):
     asyncio.set_event_loop(master_loop)
 
     try:
-        master_loop.run_until_complete(main_event_loop(delay))
+        master_loop.run_until_complete(main_event_loop(delay, show_new_messages))
     finally:
         master_loop.run_until_complete(master_loop.shutdown_asyncgens())
         master_loop.close()
 
 
-def crawler_fire_and_forget(delay: float, join_after_complete=False):
+def crawler_fire_and_forget(delay: float, join_after_complete=False, show_new_messages=True):
     loop = asyncio.get_event_loop()
     crawler_thread = threading.Thread(target=crawler_fire_and_forget_in_loop,
-                                      args=(loop, delay,))
+                                      args=(loop, delay, show_new_messages,))
     crawler_thread.start()
 
     if join_after_complete:
@@ -45,4 +45,4 @@ def crawler_fire_and_forget(delay: float, join_after_complete=False):
 
 if __name__ == '__main__':
     create_tables()
-    crawler_fire_and_forget(delay=0.0)
+    crawler_fire_and_forget(delay=0.0, show_new_messages=False)
